174954d2a31b40ca23b8dcd9080e7ba74f4e3ca8
kirphone <bulanovks@mail.ru>
2021-02-07 15:37:46 +0300

    Добавлен Cache Control

---
+++ timetable/src/main/java/ru/kirphone/timetable/interceptors/StaticFilesInterceptorConfig.java
@@ -0,0 +1,23 @@
+package ru.kirphone.timetable.interceptors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class StaticFilesInterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebContentInterceptor interceptor = new WebContentInterceptor();
+        interceptor.addCacheMapping(CacheControl.maxAge(31536000, TimeUnit.SECONDS)
+                .noTransform()
+                .mustRevalidate(), "/static/fonts/*");
+        registry.addInterceptor(interceptor);
+    }
+}
